32f9b019b6bb5d1f92f3e7f297dd10cd4046c54d
eunsookim1 <92eunsoo@gmail.com>
2023-06-09 23:04:11 -0700

    use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,7 +8,6 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
 // QUERYING THE DATABASE:
 // pool.query(`
 // SELECT id, name, cohort_id
@@ -36,13 +35,35 @@ const pool = new Pool({
 
 
 // QUERY PARAMETERS:
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `)
+//   .then(res => {
+//     res.rows.forEach(user => {
+//       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//     });
+//   }).catch(err => console.error('query error', err.stack));
+
+
+// PARAMETERIZED QUERY
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2
+  `;
+  
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit]; // Question! how come limit is not %%?
+  
+pool.query(queryString, values) // why am I separating two parameters?
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
